f8646de380507dfc48935be434312a7740520a35
Tomas Strba <tom@duckduckgo.com>
2022-05-25 14:34:43 +0200

    Empty interface

---
+++ PasswordManagerInterface/PasswordManagerInterface/Interface.swift
@@ -0,0 +1,12 @@
+//
+//  Interface.swift
+//  PasswordManagerInterface
+//
+//  Created by Tomas Strba on 25/05/2022.
+//
+
+import Foundation
+
+public protocol PasswordManagerInterface {
+
+}
---
+++ PasswordManagerInterface/PasswordManagerInterface/PasswordManagerInterface.h
@@ -0,0 +1,18 @@
+//
+//  PasswordManagerInterface.h
+//  PasswordManagerInterface
+//
+//  Created by Tomas Strba on 25/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordManagerInterface.
+FOUNDATION_EXPORT double PasswordManagerInterfaceVersionNumber;
+
+//! Project version string for PasswordManagerInterface.
+FOUNDATION_EXPORT const unsigned char PasswordManagerInterfaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordManagerInterface/PublicHeader.h>
+
+
